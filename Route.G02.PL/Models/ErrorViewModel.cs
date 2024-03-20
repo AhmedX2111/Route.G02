474873383d856834419f2f0944919bb3b2c01b0e
Jemmyx10 <ahmedkhaled910000@gmail.com>
2024-03-20 06:09:12 +0200

    Add project files.

---
+++ Route.G02.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route.G02.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
