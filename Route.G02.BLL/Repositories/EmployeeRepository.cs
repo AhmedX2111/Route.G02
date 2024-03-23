20656b87aaad89e3f276a06aa03dd78b2103f96b
Jemmyx10 <ahmedkhaled910000@gmail.com>
2024-03-23 20:51:19 +0200

    Employee Repository - BLL

---
+++ Route.G02.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.G02.BLL.Interfaces;
+using Route.G02.DAL.Data;
+using Route.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.G02.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _dbContext; // NULL
+
+        public EmployeeRepository(ApplicationDbContext dbContext) // Ask CLR for creating object from "ApplicationDbContext"
+        {
+            //_dbContext = new ApplicationDbContext(new Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext>());
+            _dbContext = dbContext;
+        }
+
+        public int Add(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Employees.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Employees.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+        {
+            //return _dbContext.Employees.Find(id);
+            return _dbContext.Find<Employee>(id); // EF Core 3.1 New Feature
+            ///var Employee = _dbContext.Employees.Local.Where(D => D.Id == id).FirstOrDefault();
+            ///
+            ///if (Employee == null)
+            ///{
+            ///    Employee = _dbContext.Employees.Local.Where(D => D.Id == id).FirstOrDefault();
+            ///}
+            ///return Employee;
+        }
+
+        public IEnumerable<Employee> GetAll()
+             => _dbContext.Employees.AsNoTracking().ToList();
+    }
+}
