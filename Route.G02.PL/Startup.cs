8afee49a86c244c6d86be921a234002fada10580
Jemmyx10 <ahmedkhaled910000@gmail.com>
2024-04-04 01:06:40 +0200

    Account Controller - SignUp

--- Route.G02.PL/Startup.cs
+++ Route.G02.PL/Startup.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Route.G02.BLL.Interfaces;
 using Route.G02.BLL.Repositories;
+using Route.G02.DAL.Models;
 using Route.G02.DAL.Data;
 using Route.G02.PL.Extensions;
 using Route.G02.PL.Helpers;
@@ -48,7 +50,25 @@ namespace Route.G02.PL
             services.AddApplicationServices();
 
             services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
-        }
+
+			services.AddScoped<UserManager<ApplicationUser>>();
+
+			services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+			{
+				options.Password.RequiredUniqueChars = 2;
+				options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequiredLength = 5;
+				options.Lockout.AllowedForNewUsers = true;
+				options.Lockout.MaxFailedAccessAttempts = 5;
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(5);
+				//options.User.AllowedUserNameCharacters = "asdfghk9876";
+				options.User.RequireUniqueEmail = true;
+
+			}).AddEntityFrameworkStores<ApplicationDbContext>();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
